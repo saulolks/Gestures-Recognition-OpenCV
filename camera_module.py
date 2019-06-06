99b27401c1cb1bc7b0195c0b003a3d9e338e6aa0
Saulo <saulolucas.gm@gmail.com>
2019-06-06 18:52:02 -0300

    Reconhecimento do tom de pele

--- camera_module.py
+++ camera_module.py
@@ -1,24 +1,97 @@
 import cv2
+import imutils
 import numpy as np
 
+import camera_module as cm
 
-def background_substraction(img):
-    fgbg = cv2.createBackgroundSubtractorMOG2()
-    fgbmask = fgbg.apply(img)
-    return fgbmask
+# video_capture = cv2.VideoCapture(0)
+# # Check success
+# if not video_capture.isOpened():
+#     raise Exception("Could not open video device")
+# # Read picture. ret === True on success
+# while True:
+#     ret, frame = video_capture.read()
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     cv2.imshow("", gray)
+#
+# # Close device
+# video_capture.release()
+
+handcascade = cv2.CascadeClassifier('./haarcascades/palm_v4.xml')
+headcascade = cv2.CascadeClassifier('./haarcascades/face.xml')
+backSub = cv2.createBackgroundSubtractorMOG2()
+kernel = np.ones((3, 3), np.uint8)
+
+def take_a_pic():
+    cap = cv2.VideoCapture(0)
+    ret = True
+
+    while ret:
+        ret, frame = cap.read()
+
+        frame = imutils.resize(frame, 600)
+        cv2.imwrite("./image/background.jpeg", frame)
+        break
+
+def camera_module(original, background):
+    # ----------------------- remove face -------------------------------
+    image = cv2.cvtColor(original, cv2.COLOR_BGR2YCR_CB)
+    background = cv2.cvtColor(background, cv2.COLOR_BGR2YCR_CB)
+
+    # image = cv2.subtract(image, background)
+
+    image_gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    faces = headcascade.detectMultiScale(image_gray, 1.3, 5)
+
+    # -------------------- background substraction -----------------------
+
+    # mask = backSub.apply(img_ycrcb)
+
+    # ------------------- splitting into 3 channels ----------------------
+    y, cr, cb = cv2.split(image)
+    y_original, cr_original, cb_original = cv2.split(image)
+
+    # ------------------------- binarization -----------------------------
+    _, y = cv2.threshold(y, 54, 163, cv2.THRESH_BINARY)
+    _, cr = cv2.threshold(cr, 131, 157, cv2.THRESH_BINARY)
+    _, cb = cv2.threshold(cb, 110, 135, cv2.THRESH_BINARY)
+
+    # ------------------------- morphology -------------------------------
+    y = cv2.morphologyEx(y, cv2.MORPH_OPEN, kernel)
+    cr = cv2.morphologyEx(cr, cv2.MORPH_OPEN, kernel)
+    cb = cv2.morphologyEx(cb, cv2.MORPH_OPEN, kernel)
+
+    # ---------------------------  merge ---------------------------------
+    y = cv2.bitwise_and(y, y_original)
+    cr = cv2.bitwise_and(cr, cr_original)
+    cb = cv2.bitwise_and(cb, cb_original)
+    img = cv2.merge((y, cr, cb))
+    img = cv2.Canny(img, 100, 200)
+
+    # ------------------------- removing face ---------------------------
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), -1)
+
+    # -------------------------- detect hands ---------------------------
+    hands = handcascade.detectMultiScale(img, 1.1, 1)
+    for (x, y, w, h) in hands:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow("", img)
+    cv2.waitKey(1)
+    # cv2.destroyAllWindows()
+
+take_a_pic()
+
+# cap = cv2.VideoCapture('./videos/libras.mp4')
+cap = cv2.VideoCapture(0)
+ret = True
+
+background = cv2.imread("./images/background.jpeg", cv2.IMREAD_COLOR)
 
 
-def split_channels(image):
-    return cv2.split(image)
+while ret:
+    ret, frame = cap.read()
 
-def binarization(image):
-    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-
-
-def morphology(image):
-    kernel = np.ones((5, 5), np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-
-
-def add(channel1, channel2, channel3):
-    return cv2.merge([channel1, channel2, channel3])
+    frame = imutils.resize(frame, 600)
+    camera_module(frame, background)
--- main.py
+++ main.py
@@ -2,43 +2,88 @@ import cv2
 import imutils
 import numpy as np
 
-import camera_module as cm
+ymin = 0
+ymax = 0
+crmin = 0
+crmax = 0
+cbmin = 0
+cbmax = 0
 
-# video_capture = cv2.VideoCapture(0)
-# # Check success
-# if not video_capture.isOpened():
-#     raise Exception("Could not open video device")
-# # Read picture. ret === True on success
-# while True:
-#     ret, frame = video_capture.read()
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     cv2.imshow("", gray)
-#
-# # Close device
-# video_capture.release()
+
+def nothing(pos):
+    pass
+
+
+def change_ymin(pos):
+    ymin = pos
+
+
+def change_ymax(pos):
+    ymax = pos
+
+
+def change_cbmin(pos):
+    cbmin = pos
+
+
+def change_cbmax(pos):
+    cbmax = pos
+
+
+def change_crmin(pos):
+    crmin = pos
+
+
+def change_crmax(pos):
+    crmax = pos
+
+
+def take_a_pic():
+    cap = cv2.VideoCapture(0)
+    ret = True
+
+    while ret:
+        ret, frame = cap.read()
+
+        frame = imutils.resize(frame, 600)
+        cv2.imwrite("./image/background.jpeg", frame)
+        break
+
 
 handcascade = cv2.CascadeClassifier('./haarcascades/palm_v4.xml')
 headcascade = cv2.CascadeClassifier('./haarcascades/face.xml')
 backSub = cv2.createBackgroundSubtractorMOG2()
-kernel = np.ones((1, 1), np.uint8)
 
-def camera_module(image):
+
+def camera_module(original, background):
+    ymin = cv2.getTrackbarPos('Ymin', 'YRB_calib')
+    ymax = cv2.getTrackbarPos('Ymax', 'YRB_calib')
+    crmin = cv2.getTrackbarPos('CRmin', 'YRB_calib')
+    crmax = cv2.getTrackbarPos('CRmax', 'YRB_calib')
+    cbmin = cv2.getTrackbarPos('CBmin', 'YRB_calib')
+    cbmax = cv2.getTrackbarPos('CBmax', 'YRB_calib')
+    wsize_open = cv2.getTrackbarPos('OpenSize', 'Windows sizes')
+    wsize_gaussian = cv2.getTrackbarPos('Gaussian', 'Windows sizes')
+
+    kernel = np.ones((wsize_open, wsize_open), np.uint8)
+    print(ymin, ymax, crmin, crmax, cbmin, cbmax)
+
     # ----------------------- remove face -------------------------------
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(original, cv2.COLOR_BGR2YCR_CB)
+    # background = cv2.cvtColor(background, cv2.COLOR_BGR2YCR_CB)
+
+    image_gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
     faces = headcascade.detectMultiScale(image_gray, 1.3, 5)
 
-    # -------------------- background substraction -----------------------
-
-    # mask = backSub.apply(img_ycrcb)
-
+    no_back = image  # original - background
+    mask = backSub.apply(original)
     # ------------------- splitting into 3 channels ----------------------
-
-    y, cr, cb = cv2.split(image)
+    y, cr, cb = cv2.split(no_back)
 
     # ------------------------- binarization -----------------------------
-    y = cv2.adaptiveThreshold(y, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 12)
-    cr = cv2.adaptiveThreshold(cr, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 12)
-    cb = cv2.adaptiveThreshold(cb, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 12)
+    _, y = cv2.threshold(y, ymin, ymax, cv2.THRESH_BINARY)
+    _, cr = cv2.threshold(cr, crmin, crmax, cv2.THRESH_BINARY)
+    _, cb = cv2.threshold(cb, cbmin, cbmax, cv2.THRESH_BINARY)
 
     # ------------------------- morphology -------------------------------
     y = cv2.morphologyEx(y, cv2.MORPH_OPEN, kernel)
@@ -47,15 +92,22 @@ def camera_module(image):
 
     # ---------------------------  merge ---------------------------------
     img = y + cr + cb
-    mask = backSub.apply(img)                                       # background substraction
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)           # opening operator
-    img = cv2.bitwise_and(image, image, mask=mask)                  # and operator
+    _, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
 
-    imutils.resize(img, 600)
+    # y, cr, cb = cv2.split(original)
+    # y *= img
+    # cr *= img
+    # cb *= img
+    # img = cv2.merge([y, cr, cb])
+    # img = cv2.bitwise_and(mask, img)
+    # img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
 
+    kernel_gaussian = np.ones((wsize_gaussian, wsize_gaussian), np.float32) / (wsize_gaussian * wsize_gaussian)
+    img = cv2.filter2D(img, -1, kernel_gaussian)
     # ------------------------- removing face ---------------------------
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), -1)
+        h += 20
+        cv2.rectangle(img, (x, y - h), (x + w, y + h), (0, 0, 0), -1)
 
     # -------------------------- detect hands ---------------------------
     hands = handcascade.detectMultiScale(img, 1.1, 1)
@@ -64,15 +116,30 @@ def camera_module(image):
 
     cv2.imshow("", img)
     cv2.waitKey(1)
-    #cv2.destroyAllWindows()
+
+    return img
 
 
-cap = cv2.VideoCapture('./videos/libras.mp4')
+take_a_pic()
+background = cv2.imread("./images/background.jpeg", cv2.IMREAD_COLOR)
+
+cap = cv2.VideoCapture(0)
 ret = True
 
-while ret:
+cv2.namedWindow('YRB_calib')
+cv2.createTrackbar('Ymin', 'YRB_calib', 54, 255, nothing)
+cv2.createTrackbar('Ymax', 'YRB_calib', 137, 255, nothing)
+cv2.createTrackbar('CRmin', 'YRB_calib', 135, 255, nothing)
+cv2.createTrackbar('CRmax', 'YRB_calib', 174, 255, nothing)
+cv2.createTrackbar('CBmin', 'YRB_calib', 0, 255, nothing)
+cv2.createTrackbar('CBmax', 'YRB_calib', 125, 255, nothing)
 
+cv2.namedWindow('Windows sizes')
+cv2.createTrackbar('OpenSize', 'Windows sizes', 1, 10, nothing)
+cv2.createTrackbar('Gaussian', 'Windows sizes', 5, 15, nothing)
+
+while ret:
     ret, frame = cap.read()
 
     frame = imutils.resize(frame, 600)
-    camera_module(frame)
+    bin_image = camera_module(frame, background)
